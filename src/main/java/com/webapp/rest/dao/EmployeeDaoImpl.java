bb7ec007bd308b532fe46d9694be29dd94f59885
Abhishek Dhilpe <abhishek.m.dhilpe@gmail.com>
2021-07-08 13:11:51 +0530

    first

---
+++ src/main/java/com/webapp/rest/dao/EmployeeDaoImpl.java
@@ -0,0 +1,23 @@
+package com.webapp.rest.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.webapp.rest.Employee;
+
+public class EmployeeDaoImpl implements EmployeeDao {
+
+	@Override
+	public List<Employee> getEmployees() {
+		
+		Employee e1= new Employee(101,"Jas");
+		Employee e2 = new Employee(102,"Kamal");
+		
+		List<Employee> lst = new ArrayList<Employee>();
+		lst.add(e1);
+		lst.add(e2);
+
+		return lst;
+	}
+	
+}
